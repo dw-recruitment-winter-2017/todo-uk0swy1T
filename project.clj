680370f7e7e9c16bd3df1eb9e6708f03dde28e6d
Anonymous Applicant <anonymous@example.com>
2017-02-22 17:56:43 -0700

    db added id todo done

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
-(defproject my-todoapp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject my-todoapp "0.1.0"
+  :description "TODO app with database"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [hiccup "1.0.2"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [com.h2database/h2 "1.3.170"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]]
   :plugins [[lein-ring "0.9.7"]]
