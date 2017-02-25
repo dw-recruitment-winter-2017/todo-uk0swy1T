15fcececbe13fdfa2b48fc21dbd263ec82ddb8a0
Anonymous Applicant <anonymous@example.com>
2017-02-25 12:26:58 -0700

    added some style

--- src/my_todoapp/views.clj
+++ src/my_todoapp/views.clj
@@ -29,20 +29,20 @@
   (hic-p/html5
    (gen-page-head "Home")
    header-links
-   [:div {:class "main"}
-   [:h1 "Home"]
-   [:p "To do list items"]
-   [:ul
+   [:div {:class "main" :id "todoapp"}
+   [:header {:id "header"}
+   [:h1 "Todos"]
+   [:form {:action "/add/" :method "POST"}
+   [:p {:id "new-todo"} [:input {:type "text" :name "x"}] [:input {:type "submit" :value "add item"}]]]
+   [:ul {:id "todo-list"}
    (for [loc all-items]
      (if (:done loc)
-       [:li {:id (str (:id loc)) :class "thisdone"} (:todo loc) [:form {:action (str "/toggle/" (:id loc)) :method "POST"}
+       [:div {:class "view"} [:li {:id (str (:id loc)) :class "thisdone"} (:todo loc) [:form {:action (str "/toggle/" (:id loc)) :method "POST"}
       [:input {:type "submit" :value "toggle"}]] [:form {:action (str "/delete/" (:id loc)) :method "POST"}
-      [:input {:type "submit" :value "delete"}]]]
-       [:li {:id (str (:id loc))} (:todo loc) [:form {:action (str "/toggle/" (:id loc)) :method "POST"}
+      [:input {:type "submit" :value "delete"}]]]]
+       [:div {:class "view"} [:li {:id (str (:id loc))} (:todo loc) [:form {:action (str "/toggle/" (:id loc)) :method "POST"}
       [:input {:type "submit" :value "toggle"}]] [:form {:action (str "/delete/" (:id loc)) :method "POST"}
-      [:input {:type "submit" :value "delete"}]]]))]
-   [:form {:action "/add/" :method "POST"}
-   [:p "task: " [:input {:type "text" :name "x"}] [:input {:type "submit" :value "add item"}]]]])))
+      [:input {:type "submit" :value "delete"}]]]]))]]])))
 
 (defn add-todo-results-page
   [{:keys [x]}]
@@ -62,30 +62,40 @@
       (hic-p/html5
        (gen-page-head "Toggle a todo")
        header-links
+       [:div {:class "main" :id "todoapp"}
+       [:header {:id "header"}
        [:h1 "Toggle a todo"]
-       [:p "You have marked " (:todo itemdet) " as " (if (false? (:done itemdet))
-       "done"
-       "incomplete") ]
+       [:div {:class "view"}
+       [:p "You have marked '" (:todo itemdet) "' as " (if (false? (:done itemdet))
+       "done."
+       "incomplete.") ]
        [:p "Back to the "
         [:a {:href (str "/")} "list"]
-        "."]))))
+        "."]]]]))))
+
 (defn delete-todo-page
   [x]
   (let [id (db/delete-todo x)]
     (hic-p/html5
      (gen-page-head "Deleted a todo")
      header-links
+     [:div {:class "main" :id "todoapp"}
+     [:header {:id "header"}
      [:h1 "Deleted a todo"]
+     [:div {:class "view"}
      [:p "Back to the "
       [:a {:href (str "/")} "list"]
-      "."])))
+      "."]]]])))
 
 (defn about-page
   []
   (hic-p/html5
     (gen-page-head "About")
     header-links
-    [:div {:class "main"}
+    [:div {:class "main" :id "todoapp"}
+    [:header {:id "header"}
     [:h1 "About"]
+    [:div {:class "view"}
     [:p "First crack at Clojure"]
-    [:p "If a task is complete it will have a line through it"]]))
+    [:p "If a task is complete it will have a line through it"]
+    [:p "Style inspired by " [:a {:href (str "http://todomvc.com/")} "todomvc.com"] "."]]]]))
